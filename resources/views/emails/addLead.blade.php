0ec198ab27f0d0ca047a42b74d1e53dd1a3adc9f
Ricardo Matos <matosonline@gmail.com>
2019-09-30 16:06:55 -0400

    updated and commented email/newLead.blade

--- resources/views/emails/addLead.blade.php
+++ resources/views/emails/addLead.blade.php
@@ -1,4 +1,6 @@
-{{$data['formData']['fName'].' '.$data['formData']['lName']}}, you have been assigned a new lead. Please <a href="{{url('/editLead/'.$data['formData']['id'])}}" title="click">click here</a> to view.
+{{-- {{$data['formData']['fName'].' '.$data['formData']['lName']}},<!-- this should be Agent Name, not Lead name -->  --}}
+
+You have been assigned a new lead. Please <a href="{{url('/editLead/'.$data['formData']['id'])}}" title="click">click here</a> to view.
 
 
 
